96eedc5aca99e782d1b70a9fb74c8615d87e98f5
Khoi Phan <jasonphan2631@gmail.com>
2023-12-16 18:56:04 -0800

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,218 @@ const butt_arr =[];
 
 var timeDisplayEl = $('#time-display');
 
+//
+//const GEO_API="AIzaSyA1lo2L-7mxPdDWeieZjKek-eBSVH9QWD0";
+const API_KEY1 = "42fc323d-4d1a-441d-a3cf-f4cf388c4ed2";
+
+/*
+const GoogleURL = `https://maps.googleapis.com/maps/api/geocode/json?address=Campbell,+CA&key=${GEO_API}`;
+fetch(GoogleURL)
+.then(function(response)
+{
+  return response.json();
+})
+.then(function(data){
+  console.log(data);
+
+})
+
+//37.2871651
+//-121.9499568
+*/
+
+function getLocation() {
+  if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+   // x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  console.log(position.coords.latitude+" "+  position.coords.longitude);
+}
+
+//getLocation();
+
+
+
+    //This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+    function calcCrow(lat1, lon1, lat2, lon2) 
+    {
+      var R = 6371; // km
+      var dLat = toRad(lat2-lat1);
+      var dLon = toRad(lon2-lon1);
+      var lat1 = toRad(lat1);
+      var lat2 = toRad(lat2);
+
+      var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      var d = R * c;
+      return d*0.621371;
+    }
+
+    // Converts numeric degrees to radians
+    function toRad(Value) 
+    {
+        return Value * Math.PI / 180;
+    }
+
+
+
+
+
+
+
+
+
+/*
+<select class="drop-down-menu" name="radius-location" id="radius-location">
+          <option value="none" selected> Choose a distance</option>
+          <option value="5miles">5 Miles</option>
+          <option value="10 miles">10 Miles</option>
+          <option value="20miles">20 Miles</option>
+        </select>
+
+
+
+        var e = document.getElementById("elementId");
+var value = e.options[e.selectedIndex].value;
+var text = e.options[e.selectedIndex].text;
+
+*/
+
+let e = document.getElementById("radius-location");
+
+
+
+
+
+function getOption() {
+
+  
+  var value = e.options[e.selectedIndex].text.split(" ");
+  return value[0];
+}
+
+function getCharingPointDetail(lat2,lon2)
+{
+
+  navigator.geolocation.getCurrentPosition(
+    position => {
+        const { latitude, longitude } = position.coords; // Get coordinates of user location
+
+       // console.log("la: "+ latitude+" lo: "+longitude);
+        const OPENCHARGE_URL = `https://api.openchargemap.io/v3/poi/?output=json&key=${API_KEY1}&latitude=${lat2}&longitude=${lon2}&levelid=3
+        &distance=${getOption()}&maxresults=5&countryid=US`
+        
+        //calcCrow(lat1, lon1, lat2, lon2) 
+
+        console.log(" value: "+getOption());
+    
+        fetch(OPENCHARGE_URL)
+        .then(function(response)
+        {
+          return response.json();
+        })
+        .then(function(data){
+          console.log(data);
+         // console.log(data[0].AddressInfo.Title)
+
+         /*
+         1551 Parkmoor Ave San Jose CA 95128
+ Distance: 5.69 miles
+ Charge Level: Level 3:  High (Over 40kW)
+Providing Capability: ChargePoint
+ Number Charging station: null
+ Cost null
+
+  for (let index = 0; index < fiveDay_forecast.length; index++) {
+       $('.card'+index+' h3').text(foreCastDataList[index].dt_txt.split(" ")[0]);
+       var url_icon = "https://openweathermap.org/img/wn/"+foreCastDataList[index].weather[0].icon+"@2x.png";
+       $('.card'+index+' img').attr('src',url_icon);
+       $(('.card'+index)+ ' .2').text((((foreCastDataList[index].main.temp- 273.15) * 9/5) + 32).toFixed(0)+"°F");
+      $(('.card'+index)+ ' .3').text(foreCastDataList[index].wind.speed);
+      $(('.card'+index)+ ' .4').text(foreCastDataList[index].main.humidity);
+
+           <li class="charger-card0">
+                <h3>........</h3> 
+                <img src="./assets/img/charging_icon.png" alt="charging-icon">
+                <h4>Level: <span class="5"></span></h4>
+                <h4>Distance from you: <span class="6"></span></h4>
+                <h4>Address: <span class="7"></span></h4>
+              </li>
+
+         */
+        
+          for (let index = 0; index < data.length; index++) {
+            //DistanceUnit
+            //data[index].AddressInfo.DistanceUnit=1;
+            /*
+            console.log(data[index].AddressInfo.AddressLine1+" "+data[index].AddressInfo.Town+" "+data[index].AddressInfo.StateOrProvince+" "+data[index].AddressInfo.Postcode)
+            console.log("Distance: "+ (calcCrow(latitude,longitude,data[index].AddressInfo.Latitude,data[index].AddressInfo.Longitude)).toFixed(2)+" miles");
+            console.log("Charge Level: "+ (data[index].Connections[0].Level.Title));
+            if(data[index].OperatorInfo!=null)
+            {
+            console.log("Providing Capability: "+ (data[index].OperatorInfo.Title));
+           
+            }
+            console.log("Number Charging station: "+ (data[index].NumberOfPoints));
+            console.log("Cost "+ (data[index].UsageCost));
+            console.log("\n");
+            */
+            $('.charger-card'+index+' img').attr('src','./assets/img/charging_icon.png');
+            $('.charger-card'+index+' h3').text((data[index].Connections[0].Level.Title)+"\n");
+           $(('.charger-card'+index)+ ' .5').text((calcCrow(latitude,longitude,data[index].AddressInfo.Latitude,data[index].AddressInfo.Longitude)).toFixed(2)+" miles\n");
+          
+          
+           if(data[index].OperatorInfo!=null)
+          {
+           $(('.charger-card'+index)+ ' .6').text((data[index].OperatorInfo.Title));
+           }
+
+
+           if (data[index].NumberOfPoints===null || data[index].UsageCost===null)
+          {
+            $(('.charger-card'+index)+ ' .7').text(" Not Available");
+            $(('.charger-card'+index)+ ' .8').text(" Not Available");
+            $(('.charger-card'+index)+ ' .9').text(data[index].AddressInfo.AddressLine1+" "+
+            data[index].AddressInfo.Town+"\n"+data[index].AddressInfo.StateOrProvince+", "+
+            data[index].AddressInfo.Postcode);
+          }
+          else
+          {
+
+           $(('.charger-card'+index)+ ' .7').text((data[index].NumberOfPoints));
+           $(('.charger-card'+index)+ ' .8').text((data[index].UsageCost));
+           
+           $(('.charger-card'+index)+ ' .9').text(" "+data[index].AddressInfo.AddressLine1+" "+
+                                                  data[index].AddressInfo.Town+"\n"+data[index].AddressInfo.StateOrProvince+", "+
+                                                  data[index].AddressInfo.Postcode);
+          }
+          }
+        })
+     
+        
+    },
+    error => { // Show alert if user denied the location permission
+        if (error.code === error.PERMISSION_DENIED) {
+            alert("Geolocation request denied. Please reset location permission to grant access again.");
+        } else {
+            alert("Geolocation request error. Please reset location permission.");
+        }
+    });
+
+    
+
+ 
+
+}
+
+
+
+
 function displayTimeDashBoard()
 {
 var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
@@ -39,6 +251,8 @@ function getWeather(citiName,latitude,longtitude)
         //console.log(data.name);
         //console.log((((data.main.temp- 273.15) * 9/5) + 32).toFixed(0)+"°F");
         //console.log(data.main.humidity);
+
+        
         $('.weather-detail h3').text(citiName.toUpperCase()+" ("+currentDate+")");
         $('.2').text((((data.main.temp- 273.15) * 9/5) + 32).toFixed(0)+"°F");
         $('.3').text(data.wind.speed);
@@ -109,10 +323,10 @@ function getCitylocation()
             return alert (`${cityName} is invalid, please re-check the input`);
       //  var namecity =data[0].name;
        // console.log("name: "+namecity);
-            //console.log(data);
+            console.log(data);
            const {name,lat,lon} =data[0];
          //    console.log("Name: "+ name);
-         //  console.log("Lat: "+lat);
+       //   console.log("Lat: "+lat);
          //   console.log("Long: "+lon);
 
          const data_object = {
@@ -139,6 +353,7 @@ function getCitylocation()
           console.log(cityList.length+" length");
            getWeather(cityName,lat,lon);
            getForecast(lat,lon);
+           getCharingPointDetail(lat,lon);
            getHistory();
 
         })
@@ -150,6 +365,7 @@ function getCitylocation()
      
 
 }
+//cityList.push(data_object);
 
 
 
@@ -161,8 +377,10 @@ const getUserCoordinates = () => {
           const API_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
           fetch(API_URL).then(response => response.json()).then(data => {
               const { name } = data[0];
+
               getWeather(name,latitude,longitude);
             getForecast(latitude,longitude);
+            getCharingPointDetail(latitude,longitude);
           }).catch(() => {
               alert("An error occurred while fetching the city name!");
           });
@@ -213,6 +431,7 @@ function getHistory() {
 
     getWeather(parsedObject[$(this).attr('id')].cityName,parsedObject[$(this).attr('id')].latitude,parsedObject[$(this).attr('id')].longtitude);
     getForecast(parsedObject[$(this).attr('id')].latitude,parsedObject[$(this).attr('id')].longtitude);
+    getCharingPointDetail(parsedObject[$(this).attr('id')].latitude,parsedObject[$(this).attr('id')].longtitude);
 
   
 	});
@@ -224,17 +443,3 @@ function getHistory() {
 
 searchButton.addEventListener('click',getCitylocation);
 current_Location.addEventListener('click',getUserCoordinates);
-
-const API_KEY1 = "42fc323d-4d1a-441d-a3cf-f4cf388c4ed2";
-const latitude1 = "37.335480";
-const longitude1 = "-121.893028";
-const OPENCHARGE_URL = `https://api.openchargemap.io/v3/poi/?output=json&key=${API_KEY1}&latitude=${latitude1}&longitude=${longitude1}&distance=5&maxresults=5`
-fetch(OPENCHARGE_URL)
-.then(function(response)
-{
-  return response.json();
-})
-.then(function(data){
-  console.log(data);
-  console.log(data[0].AddressInfo.Title)
-})
